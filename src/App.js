9f42e6dffecf472a4c338db02351545a4a656fea
wanghaixiao <haixiao.wang@neox-inc.com>
2020-08-12 18:39:57 +0800

    feat: 23424

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
   function onError(err) {
     console.log(err)
   }
-  function onSuccess(googleUser) {
-    console.log(googleUser.getAuthResponse())
-    console.log(googleUser.getBasicProfile())
+  function onSuccess(data) {
+    console.log(data)
   }
   useEffect(() => {
     fbLogIn = new FacebookLogin({
-      appId: "320868815625915",
+      client_id: "320868815625915",
+      redirect_uri: "https://www.haixiao.online"
     }, onSuccess, onError)
   }, []);
   return (
--- src/facebookLogin.js
+++ src/facebookLogin.js
@@ -1,50 +1,24 @@
-/* eslint-disable no-undef */
-/*
- * @Author: small
- * @Description: facebook login 
- * @Date: 2020-08-12 16:51:12
- * @LastEditTime: 2020-08-12 17:15:30
- * @FilePath: /line-test/src/facebookLogin.js
- */
-class FacebookLogin {
-  constructor(options, success, error) {
-    this.appId = options.appId
-    this.el = options.el
-    this.success = success
-    this.error = error
-    this.init()
+function rendomData(e) {
+  const je = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  for (var t = "", n = 0; n < e; n++) t += je[Math.floor(Math.random() * je.length)];
+  return t
+}
+
+class FaceBookLogin {
+  constructor(options) {
+    this.client_id = options.client_id
+    this.redirect_uri = options.redirect_uri
+    
   }
   init() {
-    const facebookSignInAPI = document.createElement('script')
-    facebookSignInAPI.setAttribute('src', 'https://connect.facebook.net/en_US/sdk.js')
-    facebookSignInAPI.setAttribute('crossorigin', 'anonymous')
-    facebookSignInAPI.setAttribute('async', true)
-    facebookSignInAPI.setAttribute('defer', true)
-    document.head.appendChild(facebookSignInAPI)
-    facebookSignInAPI.onload = this.InitFacebookButton.bind(this)
-  }
-  InitFacebookButton() {
-    FB.init({
-      appId            : this.appId,
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v8.0'
-    })
-  }
 
+  }
   login() {
-    FB.login(function(response) {
-      if (response.authResponse) {
-       console.log('Welcome!  Fetching your information.... ');
-       // eslint-disable-next-line no-undef
-       FB.api('/me', function(response) {
-         console.log('Good to see you, ' + response.name + '.');
-       });
-      } else {
-       console.log('User cancelled login or did not fully authorize.');
-      }
-  });
+    this.state = rendomData(12)
+    const url = `https://www.facebook.com/v8.0/dialog/oauth?
+      client_id=${this.client_id}&redirect_uri=${this.redirect_uri}&state=facebook_${this.state}&from=facebook`
+    window.location.href=url
   }
 }
 
-export default FacebookLogin;
+export default FaceBookLogin;
