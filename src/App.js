e47b0f2bd600e9612e8356dca60e754e9ccd593b
wanghaixiao <haixiao.wang@neox-inc.com>
2020-08-12 10:09:42 +0800

    dad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { init as lineInit, login as lineLogin } from '@line/liff'
+import { init as lineInit, login as lineLogin, getAccessToken as lineGetAccessToken } from '@line/liff'
 import { Button } from 'antd'
 import 'antd/dist/antd.css';
 const liffId = '1654651020-nRqoNOA9'
@@ -17,12 +17,22 @@ function handleGetUserInfo() {
 
 function init() {
   return lineInit({ liffId }).then(res=> {
+    console.log(res)
     return Promise.resolve(res)
   }).catch(err=> {
     return Promise.reject(err)
   })
 }
 function App() {
+  useEffect(() => {
+    init().then(res=> {
+      if(res) {
+        lineGetAccessToken().then(r=> {
+          console.log(res)
+        })
+      }
+    })
+  }, []);
   return (
     <div className="App">
       <Button type="primary" onClick={ handleGetUserInfo }>登录</Button>
